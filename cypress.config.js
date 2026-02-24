b19ac5979b686448d011f00da37fe9edf8fcab72
Shubhojit <snag@lstelcom.in>
2026-02-24 15:33:15 +0530

    Added github actions

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
-
+  viewportWidth: 1920,
+  viewportHeight: 1080,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
