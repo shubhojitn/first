8c9bd4712ebceede7869eb3c7ed5b0b4d0d43b86
Shubhojit <snag@lstelcom.in>
2026-02-25 14:52:48 +0530

    Fixed view profile locator

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,7 +7,7 @@ describe('Naukri Update', () => {
     cy.get('input[maxlength="100"]').type(this.data.email);
     cy.get('input[type="Password"]').type(this.data.password)
     cy.get('.btn-primary.loginButton').click();
-    cy.get('.view-profile-wrapper').click({'center': true})
+    cy.get('a[href*="profile"]').first().click({'center': true})
     cy.get('span.edit.icon').first().click();
     cy.get('.btn-dark-ot').last().click();
     cy.get('span.typ-14Medium.mod-date-val').should('contain', 'Today')
