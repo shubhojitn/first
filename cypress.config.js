4475d9aa50908285d85ea887180c056e9285b0e8
Shubhojit <snag@lstelcom.in>
2026-02-25 17:49:29 +0530

    Added code to save artifacts

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,9 @@ module.exports = defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
   failOnStatusCode: false,
+  screenshotOnRunFailure: true,
+  video: true,
+  videoCompression: 32,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
