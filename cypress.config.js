af9f44183892e198a2fa419027766f231e97e82a
Shubhojit <snag@lstelcom.in>
2026-02-25 12:37:45 +0530

    Set fail on status to false

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   allowCypressEnv: false,
   viewportWidth: 1920,
   viewportHeight: 1080,
+  failOnStatusCode: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
